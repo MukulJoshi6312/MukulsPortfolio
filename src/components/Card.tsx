9b0070ff62a10c862837a32ad655994ebcefa59f
MUKUL SHARMA <mukulsharma@MUKULs-MacBook-Air.local>
2025-01-12 22:27:22 +0530

    estimonial section completed

---
+++ src/components/Card.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import grainImage from '@/assets/images/grain.jpg'
+import { twMerge } from 'tailwind-merge'
+import {PropsWithChildren} from 'react'
+
+const Card = ({className,children}:
+    PropsWithChildren<{className?:string}>) => {
+    return (
+        <div>
+            <div className={twMerge("bg-gray-800 rounded-3xl relative z-0 after:z-10 overflow-hidden after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:pointer-events-none after:outline-white/20 p-6",className)}>
+                <div className="absolute inset-0 -z-10 opacity-5"
+                    style={{
+                        backgroundImage: `url(${grainImage.src})`
+                    }}></div>
+                    {children}
+            </div>
+        </div>
+    )
+}
+
+// px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 
+
+export default Card
