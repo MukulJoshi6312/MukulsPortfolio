a3db9554049516f7072e5572c62ff03ce19c504a
MUKUL SHARMA <mukulsharma@MUKULs-MacBook-Air.local>
2025-01-15 21:38:38 +0530

    commit

--- src/sections/MyApproach.tsx
+++ src/sections/MyApproach.tsx
@@ -61,27 +61,6 @@ const MyApproach = () => {
             ))
             }
 
-
-{/* 
-            <div className='col-span-1'>
-            <div className="relative h-[320px]  overflow-hidden rounded-3xl p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                <div className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-                <div className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-3xl bg-slate-900 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-                  Border Magic
-                </div>
-              </div>
-            </div> */}
-
-            {/* <div className='col-span-1'>
-            <div className="relative h-[320px] overflow-hidden rounded-3xl p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                <div className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-                <div className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-3xl bg-slate-900 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-                  Border Magic
-                </div>
-              </div>
-            </div> */}
-
-
           </div>
         </div>
       </div>
